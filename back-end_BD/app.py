1fec02704aefd013d5c94746530d3b00c35a96e3
Thiago Zago <thiagoszago@hotmail.com>
2024-11-20 23:21:39 -0300

    back OK

--- back-end_BD/app.py
+++ back-end_BD/app.py
@@ -37,15 +37,16 @@ def conecta_ao_banco(
         return None, None
 
 # Conectando ao banco
+
 conexao, cursor = conecta_ao_banco()
 
 #Instanciando Flask
-app = Flask(__name__)
+app = Flask(_name_)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Felipe Cabeçudo"
+    return "API Rodando e conectada ao banco"
 
 @app.route('/teste', methods=['POST'])
 def teste():
@@ -57,48 +58,56 @@ def teste():
 
 
 
-@app.route('/usuario/cadastro', methods=['POST'])
+@app.route('/usuario/cadastro', methods=['POST', 'GET'])
 def cadastro():
-    content = request.json
-
-    email = content['email']
-    senha = content['senha']
+    print("Entrou tudo")
+    if request.method == 'GET':
+        email = request.args['email']
+        senha = request.args['senha']
+    else:
+        content = request.json
+        email = content['email']
+        senha = content['senha']
 
     if email and senha:
         #VERIFICAR SE JÁ TEM CADASTRO
-        sql = "SELECT email FROM Usuarios WHERE email = ?"
-        val = (user)
+        sql = "SELECT email FROM Cadastro3 WHERE email = ?"
+        val = (email)
 
         cursor.execute(sql, val)
         user = cursor.fetchall()
+        if not user:
+            print(user)
 
         if not user: #verificar sintaxe
             #INSERT DATA INTO DATABASE AND GET TOKEN AND ID
-            sql = "INSERT INTO Usuarios (email, senha) VALUES (?, ?)" #Confirmar como vai entrar no DB
+            sql = "INSERT INTO Cadastro3 (email, senha) VALUES (?, ?)" #Confirmar como vai entrar no DB
             val = (email, senha)
 
             cursor.execute(sql, val)
             conexao.commit()
             print("Dados inseridos no DB com sucesso")
-
+            return jsonify({"status": 200})
             #Ai eu dou um select no token que ele gerar? como faz?
-
+        print("Ja tem cadastro")
+        return jsonify({"status": 500})
  #Retornar ID?
         # SE TIVER:
-            return {
-                 "statusCode": 409,
-                 "status": "error",
-                 "message": "Usuario ja cadastrado"
-                 }
+        
+        #     return {
+        #          "statusCode": 409,
+        #          "status": "error",
+        #          "message": "Usuario ja cadastrado"
+        #          }
 
-        return {
-                "statusCode": 400,
-                "status": "error",
-                "message": "Bad Request"
-                } #Qual formato do retorno?
+        # return {
+        #         "statusCode": 400,
+        #         "status": "error",
+        #         "message": "Bad Request"
+        #         } #Qual formato do retorno?
 
 
-@app.route('/usuario/cadastro2', methods=['POST'])
+@app.route('/usuario/cadastro2', methods=['POST', 'GET'])
 def cadastro2():
     content = request.json
 
@@ -114,8 +123,8 @@ def cadastro2():
     credito_disponivel = content['salario']
     profissao = content['profissao']
     deseja_investir = bool(content['deseja_investir'])
-
     # if nome and data_nasc and telefone and email and senha and cpf and cep and salario and data_salario and credito_disponivel and profissao and deseja_investir:
+    
     if nome and cpf:
         sql = "INSERT INTO Usuarios (nome, data_nasc, telefone, email, senha, cpf, cep, salario, data_salario, credito_disponivel, profissao, deseja_investir) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
@@ -149,12 +158,18 @@ def cadastro2():
         "message": "Falta de parametros"
     }
 
-@app.route('/usuario/login', methods=['POST'])
+@app.route('/usuario/login', methods=['POST', 'GET'])
 def login():
-    content = request.json
+    print("Entrou tudo")
+    if request.method == 'GET':
+        email = request.args['email']
+        senha = request.args['senha']
+    else:
+        content = request.json
+        email = content['email']
+        senha = content['senha']    # return {"statusCode": 500}
 
-    email = content['email']
-    senha = content['senha']
+    print(email, senha)
 
     if email and senha:
 
@@ -163,16 +178,20 @@ def login():
 
         cursor.execute(sql, val)
         user = cursor.fetchall()
-        if not user: #Arrumar syntaxe
-            return {
-                "statusCode": 401,
-                "status": "error",
-                "message": "Email ou senha invalidos"
-                }
+        print(str(user))
 
-        return {"statusCode": 200,
+        if not user: #Arrumar syntaxe
+            print("Nao encontrou usuario")
+            return ""
+            # return {
+            #     "statusCode": 401,
+            #     "status": "error",
+            #     "message": "Email ou senha invalidos"
+            #     }
+
+        return jsonify({"statusCode": 200,
                 "status": "success",
-                "message": "Login feito com sucesso"} #sem token
+                "message": "Login feito com sucesso"}) #sem token
 
 
 @app.route('/extrato/list', methods=['POST'])
@@ -195,7 +214,6 @@ def extrato_list():
             "preco": row[2],  
             "data_compra": row[3],
             "categoria": row[4],
-            "cpf": row[5]
         }
         extratos.append(extrato)
 
@@ -211,7 +229,7 @@ def extrato_list():
 }
 
 
-@app.route('/extrato/adicionar', methods=['POST'])
+@app.route('/extrato/adicionar', methods=['POST', 'GET'])
 def extrato_adicionar():
         content = request.json
 
@@ -219,19 +237,17 @@ def extrato_adicionar():
         preco = content['preco']
         data_compra = content['data_compra']
         categoria = content['categoria']
-        descricao = content['descricao']
-        cpf = content['cpf']
+        nome_empresa = content['nome_empresa']
 
-        if preco and data_compra and categoria and descricao and cpf:
-            sql = "INSERT INTO Extrato (preco, data_compra, categoria, descricao, cpf) VALUES (?, ?, ?, ?, ?)"   
-            val = (preco, data_compra, categoria, descricao, cpf)
+        if preco and data_compra and categoria and nome_empresa:
+            sql = "INSERT INTO Extrato (nome_empresa, preco, data_compra, categoria) VALUES (?, ?, ?, ?)"   
+            val = (nome_empresa, preco, data_compra, categoria)
             cursor.execute(sql, val)
             conexao.commit()
-            cursor.fetchall()
             return {"statusCode": 200, 
                     "status": "success",
                     "message": "Extrato adicionado com sucesso"
-}
+                    }
 
         return {
         "statusCode": 400,
@@ -270,6 +286,5 @@ def extrato_remover():
                     "message": "Deletado com sucesso"}
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', debug=True)
-
